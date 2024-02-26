68edeea55c7fb5e18dc4620852a9af4c2b4b3aa0
Victor Santos <46580792+victorstdev@users.noreply.github.com>
2024-02-26 13:24:32 -0300

    Update options.py
    Including all groups and items on comment

--- options.py
+++ options.py
@@ -1,3 +1,64 @@
+'''
+Groups:
+  "Expected",
+  "Possession",
+  "Shots",
+  "TVData",
+  "Shots extra",
+  "Passes",
+  "Duels",
+  "Defending"
+
+Statistic Items
+  "Expected":
+    - "Expected goals",
+  
+  "Possession":
+    - "Ball possession",
+    
+  "Shots":
+    - "Total shots",
+    - "Shots on target",
+    - "Shots off target",
+    - "Blocked shots",
+
+  "TVData":
+    - "Corner kicks",
+    - "Offsides",
+    - "Fouls",
+    - "Red cards",
+    - "Free kicks",
+    - "Throw-ins",
+    - "Goal kicks",
+
+  "Shots extra":
+    - "Big chances",
+    - "Big chances missed",
+    - "Counter attacks",
+    - "Counter attack shots",
+    - "Shots inside box",
+    - "Shots outside box",
+    - "Goalkeeper saves",
+    - "Goals prevented",
+
+  "Passes":
+    - "Passes",
+    - "Accurate passes",
+    - "Long balls",
+    - "Crosses",
+
+  "Duels":
+    - "Dribbles"
+    - "Possession lost",
+    - "Duels won",
+    - "Aerials won"
+
+  "Defending":
+    - "Tackles",
+    - "Interceptions",
+    - "Clearances"
+'''
+
 groups = ['Expected', 'Shots', 'TVData']
 statisticsItems = ['Expected goals', 'Shots on target', 'Shots off target', 'Corner kicks', 'Yellow cards']
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
